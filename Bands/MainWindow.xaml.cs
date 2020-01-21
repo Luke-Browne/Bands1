fd98ebb7ed15f0705a52784bb8f6dbef35693adb
S00187306 <S00187306@LAB-B1042-29>
2020-01-21 11:48:12 +0000

    Bands added

--- Bands/MainWindow.xaml.cs
+++ Bands/MainWindow.xaml.cs
@@ -24,5 +24,17 @@ namespace Bands
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Bands Band1 = new Bands("Muse", "1994", "Matt Bellamy, Chris Wolstenholme, Dominic Howard");
+            Bands Band2 = new Bands("Blur", "1988", "Damon Albarn, Graham Coxon, Alex James, Dave Rowntree");
+            Bands Band3 = new Bands("Crush 40", "1998", "Johnny Gioeli, Tomoya Ohtani, Jun Senoue");
+            Bands Band4 = new Bands("Gorillaz", "1998", "Damon Albarn, Jamie Hewlett, Paul Simonon, Mike Smith, Remi Kabaka Jr, Mick Jones, Jean-Michael Jarre, Danger Mouse");
+            Bands Band5 = new Bands("Red Hot Chilli Peppers", "1983", "John Frusciante, Chad Smith, Anthony Kliedis, Flea, Hillel Slovak, Jack Irons");
+            Bands Band6 = new Bands("Radiohead", "1985", "Thom Yorke, Jonny Greenwood, Ed O'Brien, Colin Greenwood, Philip Selway");
+
+
+        }
     }
 }
